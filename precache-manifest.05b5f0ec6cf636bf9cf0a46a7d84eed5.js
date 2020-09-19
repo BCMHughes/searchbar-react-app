28666b23969d025d3863639693d335c5347c0055
Benjamin Hughes <benjaminhughes@Benjamins-MacBook-Pro.local>
2020-09-19 08:41:32 -0500

    Updates

---
+++ precache-manifest.05b5f0ec6cf636bf9cf0a46a7d84eed5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "895e4d4fe93a303b16ab682958c114c9",
+    "url": "/searchbar-react-app/index.html"
+  },
+  {
+    "revision": "8ef6e796517f06dd2df7",
+    "url": "/searchbar-react-app/static/css/main.4a966e5b.chunk.css"
+  },
+  {
+    "revision": "3ca71b351f4a2fbf27b5",
+    "url": "/searchbar-react-app/static/js/2.de1ca534.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/searchbar-react-app/static/js/2.de1ca534.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ef6e796517f06dd2df7",
+    "url": "/searchbar-react-app/static/js/main.a25dbedb.chunk.js"
+  },
+  {
+    "revision": "de3722a0582f46431f08",
+    "url": "/searchbar-react-app/static/js/runtime-main.672b62f0.js"
+  }
+]);
